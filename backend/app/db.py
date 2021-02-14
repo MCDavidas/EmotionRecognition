36f8cd859b8ed934688fc773097cc4272b076a45
David Samohovec <buildcommunism@gmail.com>
2021-02-15 00:21:07 +0300

    Adds database schema and database manager

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,29 +4,41 @@ import logging
 import mysql.connector as mariadb
 
 
-try:
-    PASSWORD = open('./mariadb-password.txt', 'r').read()[:-1]
-except FileNotFoundError:
-    logging.error(f'Password file not found. {SECRET_PATH}')
-    sys.exit(1)
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
-try:
-    connection = mariadb.connect(
-        user='root',
-        password=PASSWORD,
-        host='localhost',
-        port=3306,
-        database='ERProject'
-    )
-except mariadb.Error as e:
-    logging.error(f'Can`t connect to mariadb {e}')
-    sys.exit(1)
 
-cursor = connection.cursor()
+class DBManager:
+    def __init__(self, database='ERProject',
+                 host="localhost", port=3306, user="root",
+                 password_file=os.path.join(DATA_PATH, 'mariadb-pw.txt')):
+        logging.info(f'Connecting to mariadb {host}, {port}, {database}')
+        pf = open(password_file, 'r')
+        self.connection = mariadb.connect(
+            user=user,
+            password=pf.read()[:-1],
+            host=host,
+            port=3306,
+            database=database,
+        )
+        pf.close()
+        self.cursor = self.connection.cursor()
+        logging.info(f'Connection with mariadb is established')
 
-try:
-    cursor.execute('SHOW TABLES')
-except mariadb.Error as e:
-    print(f'Error: {e}')
+    def __del__(self):
+        self.connection.close()
+        logging.info(f'Connection with mariadb is closed')
 
-connection.close()
+    def populate_db(self, schema_file=os.path.join(DATA_PATH, 'schema.sql')):
+        logging.info(f'Populating database')
+        for line in open(schema_file, 'r'):
+            self.cursor.execute(line)
+
+
+if __name__ == '__main__':
+    logging_format = '%(asctime)s %(levelname)s: %(message)s'
+    logging_level = logging.INFO
+    logging.basicConfig(level=logging_level, format=logging_format)
+
+    logging.info('Creating DB manager')
+    manager = DBManager()
+    manager.populate_db()
